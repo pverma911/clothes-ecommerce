b5d7d3c19c54be607f1346c4d08934e7e65014c7
pverma911 <verma.pranshu689@gmail.com>
2020-09-17 16:11:47 +0530

    added header component for logo and options menu like a navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
 
+// Pages:
+
 import HomePage from "./pages/homepage/homepage.component"
 import ShopPage from  "./pages/shop/shop.component"
+
+// Header
+
+import Header from './components/header/header.component'
+
 // Route:
 
 import {Switch,Route} from 'react-router-dom'
@@ -16,12 +23,14 @@ import {Switch,Route} from 'react-router-dom'
 function App() {
   return (
     <div>
-    <Switch>
-      <Route exact path= '/' component= {HomePage} />
-      <Route path ='/shop' component ={ShopPage} />
-    </Switch>
-      
-      {/* <HomePage /> */}
+      <Header />   {/* We put Header so it is always rendered no matter the page */}
+
+      <Switch>
+        <Route exact path= '/' component= {HomePage} />
+        <Route path ='/shop' component ={ShopPage} />
+      </Switch>
+        
+        {/* <HomePage /> */}
     </div>
   );
 }
